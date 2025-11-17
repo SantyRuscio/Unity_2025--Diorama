906b53d5756bb5c77620a6d7567d3ff95e08d312
SantyRuscio <santiruscio1237@gmail.com>
2025-11-16 23:46:38 -0300

    EfectoParpadeo

--- Assets/sCRIPS/WindySound.cs
+++ Assets/sCRIPS/WindySound.cs
@@ -8,13 +8,30 @@ public class WindySound : MonoBehaviour
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip _audioClip;
 
+    // Tiempo mínimo entre sonidos
+    [SerializeField] private float _cooldown = 1f;
+    private float _timer = 0f;
+
+    private void Update()
+    {
+        if (_timer > 0)
+            _timer -= Time.deltaTime;
+    }
+
     private void OnTriggerStay(Collider other)
     {
-
         if (!other.CompareTag("MainCamera")) return;
 
-        Debug.Log("eNTRO Camara");
-        _audioSource.PlayOneShot(_audioClip);
+        if (_timer <= 0f)
+        {
+            _audioSource.PlayOneShot(_audioClip);
+            _timer = _cooldown;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _audioSource.Stop(); 
+       
     }
 }
-
